ac0720ed8f74f98a5a736cfd14ca162a47d05d01
yjyang027 <yjyang027@tamu.edu>
2022-06-10 15:35:36 -0500

    supported .mat

--- scTenifoldXct/dataLoader.py
+++ scTenifoldXct/dataLoader.py
@@ -14,25 +14,48 @@ import pandas as pd
 from typing import Union
 from scipy import sparse
 
+
 # https://scanpy.readthedocs.io/en/stable/api.html#reading
-file_attrs = ["h5ad", "csv", "xlsx", "h5", "loom", "mtx", "txt"]
-read_fun_keys = ["h5ad", "csv", "excel", "hdf", "loom", "mtx", "text"]
+file_attrs = ["h5ad", "csv", "xlsx", "h5", "loom", "mtx", "txt", "mat"]
+read_fun_keys = ["h5ad", "csv", "excel", "hdf", "loom", "mtx", "text"] # fun from scanpy read
 
 
-def _read_counts(counts_path: str, as_sparse: bool = True, **kwargs):
+def _read_counts(counts_path: str, 
+                transpose: bool = True, 
+                **kwargs):
+
+    """Read counts file to build an AnnData.
+
+    Args:
+        counts_path (str): Path to counts file.
+        transpose (bool, optional): Whether transpose the counts.
+
+    Returns:
+        AnnData
+    """
+
     file_attr = counts_path.split(".")[-1]
     if Path(counts_path).is_file() and file_attr in file_attrs:
         print(f"loading counts from {counts_path}")
-        read_fun_key = read_fun_keys[file_attrs.index(file_attr)]
-        read_fun = getattr(sc, f"read_{read_fun_key}")  # define sc.read_{} function
-        adata = read_fun(counts_path, **kwargs).transpose() # transpose counts file
+        if file_attr == "mat":
+            import numpy as np
+            import h5py
+            f = h5py.File(counts_path,'r')
+            # print(f.keys())
+            counts = np.array(f.get('X'))
+            if transpose:
+                counts = counts.T
+            adata = sc.AnnData(counts)
+        else:
+            read_fun_key = read_fun_keys[file_attrs.index(file_attr)]
+            read_fun = getattr(sc, f"read_{read_fun_key}")  # define sc.read_{} function
+            if transpose:
+                adata = read_fun(counts_path, **kwargs).transpose() # transpose counts file
+            else:
+                adata = read_fun(counts_path, **kwargs)
     else:
         raise ValueError("incorrect file path given to counts")
-    if as_sparse:
-        print("making counts sparse...")
-        adata.X = (
-            sparse.csr_matrix(adata.X) if not sparse.issparse(adata.X) else adata.X
-        )
+
     return adata
 
 
@@ -44,19 +67,24 @@ def build_adata(
     sep="\t",
     header = None,
     log_normalize: bool = True,
+    as_sparse: bool = True,
     **kwargs,
 ):
-    """
+
+    """Load counts, metadata of genes and cells to build an AnnData input for scTenifoldXct.
     Args:
         counts_path (str): Path to counts.
         meta_gene (Union[None, str]): Path to metadata of variables.
         meta_cell (Union[None, str]): Path to metadata of Observations
         sep (str, optional): The delimiter for metadata. Defaults to "\t".
         log_normalize (bool, optional): Whether log-normalize the counts. Defaults to True.
+        as_sparse (bool, optional): Whether make the counts sparse. Defaults to True.
+        **kwargs: key words in _read_counts
 
     Returns:
         AnnData
     """
+    
     adata = _read_counts(counts_path, **kwargs)
    
     if meta_gene_path is not None and Path(meta_gene_path).is_file():
@@ -81,6 +109,12 @@ def build_adata(
         sc.pp.normalize_total(adata, target_sum=1e4)
         sc.pp.log1p(adata)
         adata.layers["log1p"] = adata.X  # save log normalized counts to adata.X
+
+    if as_sparse:
+        print("making counts sparse...")
+        adata.X = (
+            sparse.csr_matrix(adata.X) if not sparse.issparse(adata.X) else adata.X
+        )
 
     return adata
 
